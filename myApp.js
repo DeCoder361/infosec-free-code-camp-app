d9c8352e9da09424df2d3d3b94afc8d355f9ee71
Aanuoluwani <folasadeaanu@gmail.com>
2023-07-22 19:29:43 +0100

    add initial document

--- myApp.js
+++ myApp.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
+
+app.use(helmet())
+
+app.get("/", (req, res) => {
+  res.json("Hello World!")
+})
+
+app.listen(PORT, () => {
+  console.log('Starting Express server')
+})
 
 
 
